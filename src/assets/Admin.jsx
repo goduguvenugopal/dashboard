149a91c8f10a46263edf7c6b083a3282f6fa39d0
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-05-20 14:57:41 +0530

    add

--- src/assets/Admin.jsx
+++ src/assets/Admin.jsx
@@ -3,7 +3,7 @@ import '../App.css'
 import axios from 'axios'
 import { passwordContext } from '../App'
 import { useNavigate } from 'react-router-dom'
-import { api1, api2, api3, api4 } from '../../public/api'
+import { api1, api2, api3, api4 } from '../api'
 
 
 const Admin = () => {
