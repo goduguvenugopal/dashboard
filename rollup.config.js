8298cf25af299bd8a2558f03db146a9f8025c3ac
goduguvenugopal <149813252+goduguvenugopal@users.noreply.github.com>
2024-05-20 14:09:58 +0530

    add

---
+++ rollup.config.js
@@ -0,0 +1,9 @@
+// rollup.config.js
+export default {
+    input: 'src/main.js', // Entry point of your application
+    output: {
+      file: 'dist/bundle.js', // Output file
+      format: 'cjs' // Output format (CommonJS, ES module, UMD, etc.)
+    }
+  };
+  
